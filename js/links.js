9db2d44617a3977c1c3bf89c46b9bade6214c03a
Viktor <v.solodoukhin@outlook.com>
2017-05-18 21:39:49 +0200

    menu + table style

--- js/links.js
+++ js/links.js
@@ -1,5 +1,6 @@
 var links = {
   data: 'data.html',
+  data2: 'data2.html',
   contact: 'contact.html',
   welcome: 'index.html'
 };
@@ -9,10 +10,29 @@ function goToLink(url)
   window.location.href = url;
 }
 
+function displaySubMenu()
+{
+  var currentDisplay = document.getElementById('sub-menu');
+  console.log("Current display = " + currentDisplay.style.display);
+
+  if(currentDisplay.style.display === '')
+  {
+    currentDisplay.style.display = 'block';
+  }
+  else {
+    currentDisplay.style.display = '';
+  }
+}
+
 var data = document.getElementById('data');
+var data1 = document.getElementById('data-1');
+var data2 = document.getElementById('data-2');
 var welcome = document.getElementById('welcome');
 var contact = document.getElementById('contact');
 
-data.addEventListener('click', function(e){e.preventDefault();goToLink(links['data'])});
+
+data.addEventListener('click', function(e){e.preventDefault();displaySubMenu();});
+data1.addEventListener('click', function(e){e.preventDefault();goToLink(links['data'])});
+data2.addEventListener('click', function(e){e.preventDefault();goToLink(links['data2'])});
 welcome.addEventListener('click', function(e){e.preventDefault();goToLink(links['welcome'])});
 contact.addEventListener('click', function(e){e.preventDefault();goToLink(links['contact'])});
